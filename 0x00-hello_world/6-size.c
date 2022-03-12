ff45c20f29feffe21163ebaf9752418021cdd2ad
Devick001 <geovick009@gmail.com>
2022-03-12 12:40:31 -0800

    I wanna get it right

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 	long long a;
 	float f;
   
-printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
-printf("The size of a long long int: %lu byte(s)\n", sizeof(a));
-printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(a));
+printf("Size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
  
 return(0);
 }
