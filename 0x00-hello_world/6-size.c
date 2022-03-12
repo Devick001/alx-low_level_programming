89e659c9efe02f65152edb408c0c93a5a4a9244e
Devick001 <geovick009@gmail.com>
2022-03-12 09:16:00 -0800

    Sisth task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,17 @@ int main(void)
 {
 	char c;
 	int d;
+	long n;
 	double i;
+	long long a;
 	float f;
+  
+printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("The size of a double: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("The size of a long long int: %lu byte(s)\n", (Unsigned long)sizeof(a));
+printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-        printf ("The size of char is: %lu.\n", (unsigned long)sizeof(c));
-	printf ("The size of an int is: %lu.\n", (unsigned long)sizeof(d));
-	printf ("The size of a double is: %lu.\n", (unsigned long)sizeof(i));
-        printf ("The size of a float is: %LU.\n", (unsigned long)sizeof(f));
-
-	return (0);
+return(0);
 }
