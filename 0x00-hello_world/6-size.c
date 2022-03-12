7925f9b999c62d74ff323d760731fe9ba8ed148a
Devick001 <geovick009@gmail.com>
2022-03-12 09:22:26 -0800

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,7 +19,7 @@ printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
 printf("The size of a double: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("The size of a long long int: %lu byte(s)\n", (Unsigned long)sizeof(a));
+printf("The size of a long long int: %lu byte(s)\n", sizeof(a));
 printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return(0);
