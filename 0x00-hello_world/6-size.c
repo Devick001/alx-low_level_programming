50167b2a28ec5a547d35da835a4d3f4279fdf1d6
Devick001 <geovick009@gmail.com>
2022-03-12 12:36:50 -0800

    Must get it right

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-*main: Entry point
-*Getting sizes of bytes
+*main - Entry point
+*main: Getting sizes of bytes
 *
 *Description: Displays sizes of data types in bytes
 *return: Always return(0);
@@ -19,7 +19,6 @@ int main(void)
 printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
-printf("The size of a double: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("The size of a long long int: %lu byte(s)\n", sizeof(a));
 printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
  
