a15fd1dad42cd53237b3d438d9f73d0da82fb154
Devick001 <geovick009@gmail.com>
2022-03-12 10:57:48 -0800

    Trying to get it right

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- *main: Entry point
- *
- *Description: Displays sizes of data types in bytes
- *
- *return: 0
+*main: Entry point
+*
+*Getting sizes of bytes
+*
+*Description: Displays sizes of data types in bytes
+*
+*return: Always return (0)
 */
-int main(void)
+int main (void);
 {
 	char c;
 	int d;
@@ -15,12 +17,12 @@ int main(void)
 	long long a;
 	float f;
   
-printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
-printf("The size of a double: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("The size of a long long int: %lu byte(s)\n", sizeof(a));
-printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf ("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf ("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf ("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf ("The size of a double: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf ("The size of a long long int: %lu byte(s)\n", sizeof(a));
+printf ("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-return(0);
+return (0);
 }
