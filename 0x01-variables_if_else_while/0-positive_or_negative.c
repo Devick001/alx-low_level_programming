50930bac98ca1df94f8eb1ca33ddb0d5bbcda753
Devick001 <geovick009@gmail.com>
2022-03-13 03:12:35 -0700

    tas 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,21 +17,23 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-if (n > 0);
-{
-	printf("%i is positive\n", n);
-}
-else if (n < 0);
-{
-	printf("%i is negetive\n", n);
-}
-else
+if (n == 0);
 {
 	printf("%i is zero\n", n);
 }
+else if (n > 0);
+{
+	printf("%i is positive\n", n);
+}
+else
+{
+	printf("%i is negetive\n", n);
+}
+
 return(0);
 
 }
+
 
 
 	    
