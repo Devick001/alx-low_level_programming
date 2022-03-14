a0c2fbe8cdcdeaac0212a7b6d7a119225ade3c97
Devick001 <geovick009@gmail.com>
2022-03-14 06:40:51 -0700

    Hot pick

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long l;
 	float f;
 
-printf("Size of a char: %d\n", sizeof(c));
-printf("Size of an int: %d\n", sizeof(i));
-printf("Size of a long int: %d\n", sizeof(d));
-printf("size of a long long int: %zu\n", sizeof(l));
-printf("Size of a float: %d\n", sizeof(f));
+printf("Size of a char: %d bytes\n", sizeof(c));
+printf("Size of an int: %d bytes\n", sizeof(i));
+printf("Size of a long int: %d bytes\n", sizeof(d));
+printf("size of a long long int: %zu bytes\n", sizeof(l));
+printf("Size of a float: %d bytes\n", sizeof(f));
 
 return (0);
 }
