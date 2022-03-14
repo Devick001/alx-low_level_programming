be4875c9a90b84fe401cda50d54d7f8bd17cf2a0
Devick001 <geovick009@gmail.com>
2022-03-14 03:27:07 -0700

    Sizes in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,20 +5,20 @@
  * Description: Getting sizes of C data types in bytes
  * Return: zero always(success)
  */
-{
-int main(void)
 
+int main(void)
+{
 	char c;
 	int i;
 	long d;
 	long long l;
 	float f;
 
-printf("Size of a char: %c\n", sizeof(c);
-printf("Size of an int: %d\n", sizeof(i);
-printf("Size of a long int: %d\n", sizeof(d);
-printf("size of a long long int: %d\n", sizeof(l);
-printf("Size of a float: %f\n", sizeof(f);
+printf("Size of a char: %c\n", sizeof(c));
+printf("Size of an int: %d\n", sizeof(i));
+printf("Size of a long int: %d\n", sizeof(d));
+printf("size of a long long int: %zu\n", sizeof(l));
+printf("Size of a float: %f\n", sizeof(f));
 
 return(0);
 }
