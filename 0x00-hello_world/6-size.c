299d0c2db6d8d32f486ff7564fb4a0f1b382fc8c
Devick001 <geovick009@gmail.com>
2022-03-14 03:35:03 -0700

    Must get it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,12 +14,12 @@ int main(void)
 	long long l;
 	float f;
 
-printf("Size of a char: %c\n", sizeof(c));
+printf("Size of a char: %d\n", sizeof(c));
 printf("Size of an int: %d\n", sizeof(i));
 printf("Size of a long int: %d\n", sizeof(d));
 printf("size of a long long int: %zu\n", sizeof(l));
-printf("Size of a float: %f\n", sizeof(f));
+printf("Size of a float: %d\n", sizeof(f));
 
-return(0);
+return (0);
 }
 
