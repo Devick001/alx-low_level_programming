5765ca77983d0229b9334c534607da57cfad8f67
Devick001 <geovick009@gmail.com>
2022-03-15 04:34:36 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Printing a string
+ * Return:Zero(success)
+ */
+
+int main(void)
+{
+	char my_val = "_putchar";
+
+	putchar("%c\n", my_val);
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme for My C function tasks
