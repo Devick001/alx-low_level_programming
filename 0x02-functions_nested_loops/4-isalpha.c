84648567ebc7142e636952a1a44e3fdcfa392948
Devick001 <geovick009@gmail.com>
2022-03-16 07:23:44 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - Returns 1 if c is a letter, 0 otherwise.
+ * @c: contains the value to be compared
+ *
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
