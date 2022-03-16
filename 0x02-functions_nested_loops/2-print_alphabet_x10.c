b206b2438e61f0fed598dd6ddceac69510c8c63a
Devick001 <geovick009@gmail.com>
2022-03-16 06:44:46 -0700

    My commit message for task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints lower case alphabets in 10x
+ * main - Entry point
+ *
+ * Return: Always o.
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int alphaCount;
+	
+	alphaCount = 0;
+	while (alphaCount < 10)
+	{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	alphaCount++;
+
+	_alphabet('\n');
+	}
+
+}
