8031372f796701b9dfb7e74a1f3d4d62d086b5f2
Devick001 <geovick009@gmail.com>
2022-03-16 05:35:53 -0700

    Again

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * putchar - Returns binary equivalence of inputs
+ * main - Entry point
+ *
+ * Description: Takes int data by default and returns binary
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+	
+}
+
