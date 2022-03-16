b0885f264676c8154a6ce8a5eac7f08dc856d50b
Devick001 <geovick009@gmail.com>
2022-03-16 09:29:23 -0700

    My commit for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two numbers
+ * @i: print int i
+ * @k: print int k
+ * Return: 0 Always.
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
