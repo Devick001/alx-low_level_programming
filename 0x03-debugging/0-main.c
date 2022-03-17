7ca4f1a90c72cc60ec6ef0fcb403021758471d2a
Devick001 <geovick009@gmail.com>
2022-03-17 04:59:36 -0700

    Zero task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * main - main
- * Return: 0.
+ * Return: 0
  */
 
-iint main(void)
+int main(void)
 {
-        int i;
-        i = 0;
+int i;
+i = 0;
 
 positive_or_negative(i);
 
 return (0);
+
 }
