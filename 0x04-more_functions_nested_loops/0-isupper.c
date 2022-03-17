3bbec39dbecc677f17982fe7b78371b39aa0ed85
Devick001 <geovick009@gmail.com>
2022-03-17 07:19:23 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ *_isupper - Prints uppercase alphabets
+ *
+ * Description: Getting uppecae letters
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 'A'; && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
