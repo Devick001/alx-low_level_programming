9ee4e9aaf902279f2010b9f78133265e495a977b
Devick001 <geovick009@gmail.com>
2022-03-30 08:53:13 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,17 +1,41 @@
 #include "main.h"
-#include <math.h>
 
 /**
- * _pow_recursion - raises x to the power of y
- * @x: Represents the base value
- * @y: Takes the power of x
- * return:Always o.
+
+ *_pow_recursion - returns the value of x raised to the power of y
+
+ *@x: integer
+
+ *@y: integer
+
+ *Return: pow recursion
+
  */
 
-	int _pow_recursion(int x, int y)
+
+
+int _pow_recursion(int x, int y)
+
 {
-	
-		if (y < 0)
-			return (-1);
-		return _pow_recursion(pow(x, y));
+
+if (y < 0) /*Base condition*/
+
+{
+
+return (-1);
+
+}
+
+
+
+else if (y != 0)
+
+return (x * _pow_recursion(x, y - 1)); /* Recursive call*/
+
+
+
+else
+
+return (1);
+
 }
