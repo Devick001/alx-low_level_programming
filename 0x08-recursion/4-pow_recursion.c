580f44b2e769655e3e70301125001815453eca71
Devick001 <geovick009@gmail.com>
2022-03-30 07:51:47 -0700

    Task 0

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises x to the power of y
+ * @x: Represents the base value
+ * @y: Takes the power of x
+ * return:Always o.
+ */
+{
+	int _pow_recursion(int x, int y)
+	
+		if (y < 0)
+			return (-1);
+		return pow(x, y);
+}
