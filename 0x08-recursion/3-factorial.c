4c4ffba6d11df4a2e90a19efde807e28ad619432
Devick001 <geovick009@gmail.com>
2022-03-30 08:13:35 -0700

    Power function

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <math.h>
 
 /**
  * factorial - Finds the factorial of a given number
@@ -10,6 +11,10 @@
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+
 	return n * factorial(n - 1);
+	}
+	return (0);
 }
